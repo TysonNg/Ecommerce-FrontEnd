a50ca59616131490b25b0d78847cde3cab962207
NGUYEN MINH NHAT <nhatnguyenminh1507@gmail.com>
2025-04-05 08:13:46 +0700

    updated to deploy

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,12 @@
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+import {
+    FontAwesomeIcon,
+  } from "@fortawesome/react-fontawesome";
+
 export default function Loading() {
     return (
-        <div>
-            Loading....
+        <div className="w-[1200px] mx-auto my-auto">
+            <FontAwesomeIcon icon={faSpinner} spin/>
         </div>
     );
 }
